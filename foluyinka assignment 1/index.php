28591e1b9f7299b4ecebeca6d537451dda30d85c
Fiyin Oluyinka <fiyinoluyinka@yahoo.com>
2018-02-05 11:15:22 -0500

    Assignment 1 Folder Oluyinka

---
+++ foluyinka assignment 1/index.php
@@ -0,0 +1,76 @@
+<?php include "inc/html-top.inc"; ?>
+	<body>
+
+  <section class ="banner">
+  <header>
+    <a href = "index.php">
+    <h1> Fiyinfoluwa O Oluyinka </h1>
+        <img src="images/inventors.jpg" alt=" Circuit">
+        </a>
+      <p> Electrical Engineer and Research Professional </p> 
+      <?php include "inc/nav.inc"; ?>
+    </header>
+    </section>
+
+    <div class = "container">
+
+
+		<article class = item2>
+			<h2> About Me </h2>
+
+			<figure>
+				<img src= "images/nsbe.jpg" alt = "with Jack Dorsey">
+				<figcaption> Fiyin at the 42nd Annual NSBE convention </figcaption>
+			</figure>
+
+			<p> I am <strong> Fiyinfoluwa Oluyinka </strong>(born November 29, 1995) in Lagos Nigeria. I am from a family of six consisting of my parents, my 3 siblings, and myself. I began boarding school (at Loyola Jesuit College Abuja) at the age of 10, and six years later, upon my graduation, I moved to India, where I attended the Mahindra United World College. It was during my time at UWC, with friends from over 80 different countries, I learned to appreciate people's cultures, opinions and beliefs. It was also at UWC that I got my first taste into the world of Engineering. I worked with my tema members on a go-kart project. My experiences, coupled with my passion for electronics prompted me to apply to the University of Rochester, to pursue a bachelor's in Electrical and Computer Engineering. </p>
+
+			<p>  I am  a strong research professional pursuing a Bachelor’s Degree in Electrical and Computer Engineering from University of Rochester. Over the years, I have developed strong analytical skills from tackling various projects ranging from circuit analysis and design, to probability and statistics. I am very interested in Financial Technology, Electronics Hardware Design, Power, and pretty much any opportunity for me to use technology as a tool to improve business performances, mitigate risk, and provide innovative solutions to meet project demands</p>
+
+
+			<h2> Work Experience </h2>
+			<h3> ExxonMobil </h3>
+			<figure>
+				<img src= "images/exxon.jpg" alt = "Exxon">
+				<figcaption> Onsite at Exxon </figcaption>
+			</figure>
+
+			<p>During the summer of my Freshman year, I worked for Mobil Producing Nigeria Unlimited; one of ExxonMobil's Upstream affiliates in Nigeria. Here, I was a member of the offshore field redevelopments', slot addition and riser repairs project team and I assisted in making feasibility reports for scheduled field redevelopment projects. I performed thorough reconciliation of materials in order to ensure that materials left over at the end of production jobs were accurately entered into the system and returned to their appropriate storage locations. During my time with the team, I was opportune to visit fabrication yards, where I witnessed the production of surfer ladders, prior to their installment offshore. I also participated in the Factory Acceptance test of a wellhead control panel, prior to its installation on a platform offshore. </p>
+
+			<h3> Xerox Engineering Research Fellowship </h3>
+			<figure>
+				<img src= "images/xerox.jpg" alt = "Poster Presentation">
+				<figcaption> My Research Poster at the Research Symposium </figcaption>
+			</figure>
+			<p>In the summer of my Junior year, I worked as a research fellow in the University of Rochester’s Laboratory for Laser Energetics as a part of the Xerox Engineering Fellows Program. I assisted Dr. Roman Sobolewski and his team with their research on terahertz photonics and nano device characterization. Over the course of the fellowship, I performed experiments to explore the interaction between carbon nanotube samples and lasers within the Terahertz frequency range and I really enjoyed it. With the data obtained from these experiments, I calculated necessary parameters such as the conductivity, absorption coefficient and permittivity of the samples using MATLAB. However, considering the relatively short (8 weeks) duration of the program, I was only able to reach a preliminary conclusion. This prompted me to sign up for the Semiconductor Devices advanced elective, and to begin my Independent study in terahertz photonics in the fall semester of my senior year, in order for me to further develop my research and analytical skills, and to assist the team in fully understanding our samples, and discovering potential ground-breaking applications for them in industries ranging from security to medicine. </p>
+
+
+		</article>
+
+		<aside class =  "item3">
+
+			<h3> KPMG </h3>
+
+
+			<p>In my sophomore year, I had the opportunity to intern in the Technology Advisory unit of KPMG Nigeria, where I worked as a member of the project implementation team managing Fidelity Bank Nigeria’s core banking platform upgrade. On this engagement, I tracked issues experienced by the bank staff nationwide during simulations of the new platform, and collaborated with the bank’s technical team to ensure the issues were resolved within the required timeframe. I also prepared project assessment reports, for the project team as well as the Bank's Executive board, to keep them up to speed with the project’s progress. Overall, I learned how to work in a deadline-oriented environment, enhanced my collaborative skills, became adept in the use of Microsoft Excel as a tool for data analytics.</p>
+
+
+			<h2> Extra Curricular Activities </h2>
+
+			<figure>
+				<img src= "images/high.jpg" alt = "High Jump">
+				<figcaption> Outdoor High Jump at St. John Fisher College </figcaption>
+			</figure>
+
+			<p> Extending beyond my academics, my time as an undergraduate student has been a wonderful learning experience. As a sprinter on the university’s track team, I understand the value of teamwork, and I have a competitive spirit which drives me to achieve my goals. I have developed very good time management skills as result of balancing practices, travelling, my academic course load, and my extracurricular activities. I also served as workshop leader for two signals and circuits classes (ECE 111 and ECE 113), and a laboratory teaching assistant for the electromagnetic waves course (ECE 230). From these experiences, I was able to further understand the concepts covered in these classes, and from interacting with the students, I developed strong interpersonal and group facilitating skills.</p>
+			
+		</aside>
+
+	<?php include "inc/footer.inc"; ?>
+
+	</div>
+
+	<?php include "inc/scripts.inc"; ?>
+
+	</body>
+
---
+++ foluyinka assignment 1/role.php
@@ -0,0 +1,50 @@
+<?php include "inc/html-top.inc"; ?>
+  <body>
+
+  <section class ="banner">
+  <header>
+    <a href = "index.php">
+    <h1> Fiyinfoluwa O Oluyinka </h1>
+        <img src="images/inventors.jpg" alt=" The Inventors">
+        </a>
+        <p> Electrical Engineer and Research Professional </p> 
+      <?php include "inc/nav.inc"; ?>
+    </header>
+    </section>
+
+    <div class = "container">
+
+    <article class = "itemm" >
+			
+			<h2>
+				Information Architect
+			</h2>
+				<h3> Who is an Information Architect ? </h3>
+				<p>
+					The information Architecture of a webpage is very important. Essentially, on a web-based project or website, the Information architect prepares the content, and arranges them within each page, and across all related webpages. The IA designs the navigation elements and how navigation from page to page is presented, ensuring that is correctly presented in any browser, on any device, irrespective of the size. Finally, the IA leads the team collaboration (Github) and manages the file structures.
+				</p>
+
+					<p> Essentially it is the responsibility of the Information Architect to ensure that : </p>
+
+					<ul>
+						<li> There are no usability problems </li>
+						<li> The structure layer (content) is coded in HTML so it is arranged and accessible in a way that makes sense for the content </li>
+						<li> Content structure (HTML) meets industry standards and best practices including W3C HTML validation within reason </li>
+						<li> The team is coordinated and effective with no complaints </li>
+
+					</ul>
+				<p>
+					Personally, I chose this because I see this as an opportunity to improve upon my organization and structural skills, thus helping me become a neater web developer, and to further improve upon my overall understanding and conceptualization of HTML, CSS, PHP and Javascript.
+				</p>
+
+				
+		</article>
+
+
+		<?php include "inc/footer.inc"; ?>
+
+		</div>
+
+		<?php include "inc/scripts.inc"; ?>
+
+		</body>
