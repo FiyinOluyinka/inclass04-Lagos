65e37e1e9ebad3313c6a52ca5600b3d409cb5909
Olatunde Oshunlaja <oshunlajaolatunde@gmail.com>
2018-04-02 12:36:56 -0400

    Add files via upload

---
+++ assignment4-design/form.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Assignment 4</title>
+	<style>
+	/* Just some appearance adjustments... */
+	h1, h2 { margin: 0.5em 0 0.25em; }
+	h1 { font-size: 1.5em; }
+	h2 { 
+		font-size: 1em;
+		font-weight: normal;
+	}
+	fieldset { 
+		margin-bottom: 10px; 
+		width: 326px;
+	}
+	textarea { 
+		display: block; 
+		width: 350px;
+		height: 7rem;
+	}
+
+	/* The square brackets is a way to target just
+		the SUBMIT button with styles */
+	input[type="submit"] { margin-top: 10px; }
+
+	/* Secret sauce (below) - apply the class="box"
+		to any container and the elements inside will 
+		line-up nicely, side-by-side.
+		
+		USAGE: wrap all the label/input pairs you 
+		create in their own DIV and apply the 
+		class="box" to the DIV.
+	*/
+	.box { display: table-row; }
+	.box > * { 
+		display: table-cell; 
+		padding-right: 10px; 
+		margin-bottom: 10px;
+	}
+
+	</style>
+</head>
+
+<body>
+
+<h1> Contact Us </h1>
+	<form method="post" action="welcome.php">
+
+		<div class="box">
+			Name:      
+			<input type="text" name="name" id="name" required autofocus>
+			<label for="name"></label>
+		</div>
+
+		<div class="box">
+			Email:     
+			<input type="text" name="email" id="email" required>
+			<label for="email"></label>
+		</div>
+
+		<div class="box">
+			Telephone:
+			<input type="text" name="phone" id="phone" required>
+			<label for="phone"></label>
+		</div>	
+
+<fieldset>
+	
+		
+Describe what your major is and your reason for choosing it
+		<textarea name="textarea" id="textarea">
+		</textarea>
+		<input type="submit" value="Send">
+
+</form>
+
+</body>
+</html>
